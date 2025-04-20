e34657c2da8ebe14efb3a4ae069491b186b8a53a
Esha Patel <Patellesha11@gmail.com>
2025-04-20 17:45:04 +0300

    added else

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -4,6 +4,10 @@ import "./Scroll.css";
 function Scroll () {
     const [visible, setVisible] = useState(false)
     const toggleVisibility = () => {
-        
+        if (window.scrollY > 200) {
+            setVisible(true);
+         } else {
+
+            }
     }
 }
